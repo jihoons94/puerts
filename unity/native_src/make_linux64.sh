c013e19a3732afc827f6e8b371c045dfca00e3b1
jh.Kim <jh.kim@morph.kr>
2022-07-12 11:52:03 +0900

    update

--- unity/native_src/make_linux64.sh
+++ unity/native_src/make_linux64.sh
@@ -1,6 +1,18 @@
-mkdir -p build_linux64 && cd build_linux64
-cmake ../
+ENGINE=$1
+if [ "$1" == "" ]
+then
+    ENGINE="v8"
+fi
+
+CONFIG=$2
+if [ "$2" == "" ]
+then
+    CONFIG="Release"
+fi
+
+mkdir -p build_linux64_$ENGINE && cd build_linux64_$ENGINE
+cmake -DJS_ENGINE=$ENGINE -DCMAKE_BUILD_TYPE=$CONFIG ../
 cd ..
-cmake --build build_linux64 --config Release
+cmake --build build_linux64_$ENGINE --config $CONFIG
 mkdir -p ../Assets/Plugins/x86_64/
-cp build_linux64/libpuerts.so ../Assets/Plugins/x86_64/libpuerts.so
+cp build_linux64_$ENGINE/libpuerts.so ../Assets/Plugins/x86_64/libpuerts.so
--- unity/native_src/make_win32.bat
+++ unity/native_src/make_win32.bat
@@ -1,6 +1,19 @@
-mkdir build32 & pushd build32
-cmake -G "Visual Studio 16 2019" -A Win32 ..
+set ENGINE=%1
+if "%ENGINE%"=="" (
+  set ENGINE=v8
+)
+set CONFIG=%2
+if "%CONFIG%"=="" (
+  set CONFIG=Release
+)
+if "%CONFIG%"=="Debug" (
+  set CONFIG=RelWithDebInfo
+)
+
+mkdir build32_%ENGINE% & pushd build32_%ENGINE%
+cmake -DJS_ENGINE=%ENGINE% -DCMAKE_BUILD_TYPE=%CONFIG% -G "Visual Studio 16 2019" -A Win32 ..
 popd
-cmake --build build32 --config Release
+cmake --build build32_%ENGINE% --config %CONFIG%
 md ..\Assets\Plugins\x86
-copy build32\Release\puerts.dll ..\Assets\Plugins\x86
+copy build32_%ENGINE%\%CONFIG%\puerts.dll ..\Assets\Plugins\x86
+xcopy %ENGINE%\Lib\Win32\*.dll ..\Assets\Plugins\x86 /C
