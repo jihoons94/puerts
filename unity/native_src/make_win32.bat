4c000c866ad0ff8a18544fb5ea69a89846c50f10
jh.Kim <jh.kim@morph.kr>
2022-07-12 12:05:27 +0900

    update

--- unity/native_src/make_win32.bat
+++ unity/native_src/make_win32.bat
@@ -2,18 +2,11 @@ set ENGINE=%1
 if "%ENGINE%"=="" (
   set ENGINE=v8
 )
-set CONFIG=%2
-if "%CONFIG%"=="" (
-  set CONFIG=Release
-)
-if "%CONFIG%"=="Debug" (
-  set CONFIG=RelWithDebInfo
-)
 
 mkdir build32_%ENGINE% & pushd build32_%ENGINE%
-cmake -DJS_ENGINE=%ENGINE% -DCMAKE_BUILD_TYPE=%CONFIG% -G "Visual Studio 16 2019" -A Win32 ..
+cmake -DJS_ENGINE=%ENGINE% -G "Visual Studio 16 2019" -A Win32 ..
 popd
-cmake --build build32_%ENGINE% --config %CONFIG%
+cmake --build build32_%ENGINE% --config Release
 md ..\Assets\Plugins\x86
-copy build32_%ENGINE%\%CONFIG%\puerts.dll ..\Assets\Plugins\x86
+copy build32_%ENGINE%\Release\puerts.dll ..\Assets\Plugins\x86
 xcopy %ENGINE%\Lib\Win32\*.dll ..\Assets\Plugins\x86 /C
